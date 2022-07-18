14cbd92fb70678b36677d92191a288991c7b538b
glowxq <glowxq@yeah.net>
2022-07-18 23:18:21 +0800

    修改url

--- vue.config.js
+++ vue.config.js
@@ -35,7 +35,8 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://localhost:8080`,
+        target: `http://106.52.58.11:8080`,
+        // target: `http://127.0.0.1:8080`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
