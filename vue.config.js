3ff448aa68f3ba19a9ab41cfa2dcd92d7d8fb1a8
glowxq <glowxq@yeah.net>
2022-08-19 20:59:28 +0800

    修改url

--- vue.config.js
+++ vue.config.js
@@ -35,7 +35,8 @@ module.exports = {
         proxy: {
             // detail: https://cli.vuejs.org/config/#devserver-proxy
             [process.env.VUE_APP_BASE_API]: {
-                target: `http://106.52.58.11:12574`,
+                // target: `http://106.52.58.11:12574`,
+                target: `https://tinyvoice.glowxq.com`,
                 // target: `http://127.0.0.1:8080`,
                 changeOrigin: true,
                 pathRewrite: {
